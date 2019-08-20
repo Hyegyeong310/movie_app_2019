e6fbe23f29001866abbfb501757aa6b564db23f8
Hyegyeong310 <hyegyeong310@gmail.com>
2019-08-21 03:25:13 +0900

    [Add] App, Movie style 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   state = {
@@ -23,20 +24,27 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? 'Loading...'
-          : movies.map(movie => (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
               <Movie
                 id={movie.id}
                 year={movie.year}
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
                 key={movie.id}
               />
             ))}
-      </div>
+          </div>
+        )}
+      </section>
     );
   }
 }
