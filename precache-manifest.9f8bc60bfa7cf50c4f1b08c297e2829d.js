35f718cd7c2c4d4ccf02d62970d6d7a123fd4eef
Hyegyeong310 <hyegyeong310@gmail.com>
2019-08-21 03:21:45 +0900

    Updates

---
+++ precache-manifest.9f8bc60bfa7cf50c4f1b08c297e2829d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c06982642ac5709a81e9c2fe94008d",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "1ce09ccfdf8b4c54feb1",
+    "url": "/movie_app_2019/static/css/main.6345c67a.chunk.css"
+  },
+  {
+    "revision": "3350fcf69a8fdf7d4a1c",
+    "url": "/movie_app_2019/static/js/2.1c4cb735.chunk.js"
+  },
+  {
+    "revision": "1ce09ccfdf8b4c54feb1",
+    "url": "/movie_app_2019/static/js/main.a87c88d8.chunk.js"
+  },
+  {
+    "revision": "8e34f0f3f9415f236696",
+    "url": "/movie_app_2019/static/js/runtime~main.97c70ab9.js"
+  }
+]);
